6fd4e6170df32b88f17ca6b92edadba087a02ddf
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2022-07-10 15:28:14 -0400

    added dynamic grid

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const container = document.createElement('div');
-const body = document.querySelector('body');
+let color = 'black';
+function createBoard(size) {
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
 
-container.classList.add('container');
-body.appendChild(container);
+  squares.forEach((div) => div.remove());
 
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement('div');
-  div.classList.add('square');
-  container.appendChild(div);
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // makes 16 or size amt of cols and each column  is 1/16 of the container
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`; // makes 16  or size amt of rows and each row  is 1/16 of the container
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'black';
+    board.insertAdjacentElement('beforeend', square);
+
+    square.addEventListener('mouseover', colorSquare);
+  }
 }
+
+createBoard(16);
+
+function changeBoard(input) {
+  if (input >= 2 && input <= 100) {
+    createBoard(input);
+  } else {
+    alert('must enter a value between 2 and 100');
+  }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = color;
+}
+
+function changeColor()
